e3373ef51735d9a122406d9f5c8f1d96aa9c3408
Elias <eliasgalindo@gmail.com>
2022-01-25 17:03:38 -0300

    Requisito 1 - getSpeciesByIds

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -3,6 +3,7 @@ const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
   species.find((specie) => specie.name === animal);
+  species.forEach((ag) => species.residents.age);
 }
 
 module.exports = getAnimalsOlderThan;
