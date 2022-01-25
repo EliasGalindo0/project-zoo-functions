3143a460bb72b74fd56de90fc43e414b728f0c1d
Elias <eliasgalindo@gmail.com>
2022-01-25 17:14:00 -0300

    Requisito 1 - getSpeciesByIds

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -2,10 +2,10 @@ const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 const getSpeciesByIds = (...ids) => { //operador rest para poder receber mais de um parâmetro.
-  if(!ids) { //comparador que indica que tudo aquilo for deiferente de 'ids' retornará 
+  if(!ids) { //comparador que indica que tudo aquilo for diferente de 'ids' retornará um array vazio.
     return [];
   }
-  return species.filter((specie) => ids.includes(specie.id));
+  return species.filter((specie) => ids.includes(specie.id)); //a verificação do valor inserido como parâmetro no ID será realizada pelo 'includes', se ele retornar true, ele retornará o elemento que seja igual ao ID inserido.
 };
 
 module.exports = getSpeciesByIds;
