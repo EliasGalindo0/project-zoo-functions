9fbd6921e7e4de8523682001e55a665d7c6d0139
Elias <eliasgalindo@gmail.com>
2022-01-25 19:46:13 -0300

    Refactoring getAnimalsOlderThan

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,10 +1,8 @@
 const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getAnimalsOlderThan(animal, age) {
-  return species.find((specie) => specie.name === animal)
-    .residents.every((resAge) => resAge.age >= age);
-}
+const getAnimalsOlderThan = (animal, age) => species.find((specie) => specie.name === animal)
+  .residents.every((resAge) => resAge.age >= age);
 
 module.exports = getAnimalsOlderThan;
 
