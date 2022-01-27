45778d4b7b8770034fb54daf8c583d98ff2ff4f1
Elias <eliasgalindo@gmail.com>
2022-01-27 17:20:02 -0300

    Requisito 5 - countAnimals

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,19 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function countAnimals(animal) {
-  // seu código aqui
-}
+const countAnimals = (animal) => {
+  const auxObject = {};
+  if (!animal) {
+    species.forEach((specie) => {
+      auxObject[specie.name] = specie.residents.length;
+    });
+    return auxObject;
+  }
+  if (!animal.sex) {
+    return species.find((specie) => specie.name === animal.specie).residents.length;
+  }
+  return species.find((specie) => specie.name === animal.specie).residents.filter((specie) =>
+    specie.sex === animal.sex).length;
+};
 
 module.exports = countAnimals;
