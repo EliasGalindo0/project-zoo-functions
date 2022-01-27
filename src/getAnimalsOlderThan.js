b2aed36fe7e13fab6588bc2248140d312942484d
Elias <eliasgalindo@gmail.com>
2022-01-26 21:56:36 -0300

    Requisito 4 - getRelatedEmployees

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -2,6 +2,6 @@ const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 const getAnimalsOlderThan = (animal, age) => species.find((specie) => specie.name === animal)
-  .residents.every((resAge) => resAge.age >= age);
+  .residents.every((residentAge) => residentAge.age >= age);
 
 module.exports = getAnimalsOlderThan;
--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,14 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function isManager(id) {
-  // seu código aqui
-}
+const isManager = (id) => employees.some((employee) => employee.managers.includes(id));
 
-function getRelatedEmployees(managerId) {
-  // seu código aqui
-}
+const getRelatedEmployees = (managerId) => {
+  if (!isManager(managerId)) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+  return employees.filter((employee) => employee.managers.includes(managerId))
+    .map((name) => `${name.firstName} ${name.lastName}`);
+};
 
 module.exports = { isManager, getRelatedEmployees };
