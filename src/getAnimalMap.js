3479ad2e87c879a39b357c21ffb298c8ebcb72ec
Elias <eliasgalindo@gmail.com>
2022-01-28 23:11:32 -0300

    Requisito 7 - getAnimalMap

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,37 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getAnimalMap(options) {
-  // seu código aqui
-}
+const sortBySex = (options, residents) => residents
+  .filter((resident) => resident.sex === options.sex)
+  .map((resident) => resident.name);
+
+const getNames = (options, residents) => {
+  let names = residents.map((resident) => resident.name);
+  if (options.sex) {
+    names = sortBySex(options, residents);
+  }
+  if (options.sorted) {
+    names = names.sort();
+  }
+  return names;
+};
+
+const getAnimalMap = (options) => {
+  const cardinalLocation = {};
+  species.forEach(({ location }) => { cardinalLocation[location] = []; });
+  if (options && options.includeNames) {
+    species.forEach(({ name, location, residents }) => {
+      const specieName = {};
+      specieName[name] = getNames(options, residents);
+      cardinalLocation[location].push(specieName);
+    });
+    return cardinalLocation;
+  }
+  species.forEach(({ name, location }) => { cardinalLocation[location].push(name); });
+  return cardinalLocation;
+};
 
 module.exports = getAnimalMap;
+
+console.log(getAnimalMap({ includeNames: true, sorted: true }));
+// // console.log(includeNames({ includeNames: true }));
